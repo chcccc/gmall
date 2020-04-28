d8262c2b2d55253610c4dbfedf78723da923e477
chcccc <2271020426@qq.com>
2020-04-28 19:24:36 +0800

    工程初步提交

---
+++ gmall-core/src/main/java/cn/chmcyz/gmall/core/bean/UserInfo.java
@@ -0,0 +1,15 @@
+package cn.chmcyz.gmall.core.bean;
+
+import lombok.Data;
+
+/**
+ * @author jiangli
+ * @since 2020/1/29 14:24
+ */
+@Data
+public class UserInfo {
+
+	private String userKey;
+
+	private Long id;
+}
